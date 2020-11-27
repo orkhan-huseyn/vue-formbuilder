2830de9171803908126bc4de4715ca6193cb26fe
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-11-27 16:29:35 +0400

    remove vue lodash

--- src/main.js
+++ src/main.js
@@ -1,63 +1,49 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-
+import Vue from "vue";
 
 // ================
 // Use Element UI
 // ----------------
-import Element from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en' // Default lang is Chinese
-import './assets/scss/_var.scss'
-Vue.use(Element, { locale })
-import './assets/scss/main.scss'
+import Element from "element-ui";
+import locale from "element-ui/lib/locale/lang/en"; // Default lang is Chinese
+import "./assets/scss/_var.scss";
+Vue.use(Element, { locale });
+import "./assets/scss/main.scss";
 
-
-import App from './App'
+import App from "./App";
 
 // ================
 // Transitions
 // ----------------
-import Animate from 'vue2-animate/dist/vue2-animate.min.css'
-Vue.use(Animate)
-
-// ================
-// Lodash
-// ----------------
-import VueLodash from 'vue-lodash'
-Vue.use(VueLodash)
+import Animate from "vue2-animate/dist/vue2-animate.min.css";
+Vue.use(Animate);
 
 // ================
 // Use Vue Router
 // ----------------
-import router from './router'
-
+import router from "./router";
 
 // ================
 // Use Layouts
 // ----------------
-import Default from './layouts/Default'
-Vue.component('default-layout', Default);
-
+import Default from "./layouts/Default";
+Vue.component("default-layout", Default);
 
 // ================
 // Vue-stash aka simple vuex alternative
 // ----------------
-import VueStash from 'vue-stash'
-import store from './store/store'
-Vue.use(VueStash)
+import VueStash from "vue-stash";
+import store from "./store/store";
+Vue.use(VueStash);
 
-
-
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 var vm = new Vue({
-  el: '#app',
+  el: "#app",
   router,
-  components: { App },
   data: { store },
-  template: '<App/>',
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
 
 global.vm = vm;
